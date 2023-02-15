82175d954b14250091081ae8a02b861403ea9de6
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-15 15:14:05 +0530

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpWageWithOops
+{
+    public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Employee Wage computation problem");
+    }
+}
+}
