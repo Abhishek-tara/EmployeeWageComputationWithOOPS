11b6d11e5fd516f6580fc380e35496eb4746726d
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-15 15:41:50 +0530

    Added Program to calculate Part time Employee wage Using Switch case

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Employee Wage computation problem");
+
+            UseSwitchCase partTime = new UseSwitchCase();
+            partTime.calPartTime();
     }
 }
 }
