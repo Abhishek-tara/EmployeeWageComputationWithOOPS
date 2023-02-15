462cb230e607bd046198a09d486fea4d4fa941dc
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-15 15:22:35 +0530

    Added Program to check employee is present or absent

---
+++ CheckEmployee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageWithOops
+{
+    public class CheckEmployee
+    {
+        public int IS_FULLTIME = 1;
+        public  void EmployeeCheck()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULLTIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Employee Wage computation problem");
+
+        CheckEmployee check = new CheckEmployee();
+
+        check.EmployeeCheck();
     }
 }
 }
