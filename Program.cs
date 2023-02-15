6c811559e80a74925dd26cd0f543cb5b09029fd2
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-15 15:47:20 +0530

    Added Program to calculate wages for month

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Employee Wage computation problem");
+
+            CalculatingWagesForMonth wage = new CalculatingWagesForMonth(); 
+            wage.calCulateWage();
     }
 }
 }
