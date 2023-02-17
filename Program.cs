3a3c59321ba3075c4f1317bdb3e5608a577d5830
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-17 13:13:29 +0530

    Added Program to compute wage for multiple company

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
 ﻿namespace EmpWageWithOops
 {
     public class Program
-{
-    public static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Employee Wage computation problem");
+        public static void Main(string[] args)
+        {
+             Console.WriteLine("Welcome to the Employee Wage computation problem");
+
+             ComputeWage Company = new ComputeWage();
+            Company.calculateWage("StarBucks",15, 20, 60);
+
+            
+            Company.calculateWage("Apple",25, 22, 70);
+
+
+        }
     }
-}
 }
