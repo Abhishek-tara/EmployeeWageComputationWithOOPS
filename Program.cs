1cfed9a328c74cf9e9d0336fd5adb9d4943d13ba
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-17 12:45:44 +0530

    Refactor Code to calculate wage

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Employee Wage computation problem");
+        
+        ComputeEmpWage wage= new ComputeEmpWage();
+        wage.calculateWage();
     }
 }
 }
