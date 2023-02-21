a857056228757df9c4df5bb36f7f987a310f70f7
Abhishek-tara <abhishektara.ak47@gmail.com>
2023-02-21 19:38:00 +0530

    Added Program to Sace Total Wage for Each Company

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage computation problem");
+
+            EmpWageBuilder Samsung = new EmpWageBuilder("SAMSUNG", 20, 5, 25);
+            EmpWageBuilder Apple = new EmpWageBuilder("APPLE", 16, 6, 20);
+
+            Samsung.computeEmpWage();
+            Console.WriteLine(Samsung.toString());
+            Apple.computeEmpWage();
+            Console.WriteLine(Apple.toString());
         }
     }
 }
